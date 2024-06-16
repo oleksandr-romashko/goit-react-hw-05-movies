3d79cd4dc9aa4aefc1639515943ec66308234da9
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-16 20:28:47 +0200

    Refactor search form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,6 @@
 import css from "./SearchForm.module.css";
 
-const SearchForm = ({searchQuery, onQueryChange, onSearch}) => {
-  const handleSearchQueryChange = (event) => {
-    onQueryChange(event.target.value);
-  };
-
+const SearchForm = ({onSearch}) => {
   const handleSearch = (event) => {
     event.preventDefault();
     const query = event.target.searchQuery.value;
@@ -15,19 +11,18 @@ const SearchForm = ({searchQuery, onQueryChange, onSearch}) => {
   return (
     <form 
       className={css["search-form"]} 
-      onSubmit={handleSearch}>
-        <input 
-          value={searchQuery} 
-          onChange={handleSearchQueryChange} 
-          name="searchQuery" 
-          type="text" 
-          className={css["search-query-input"]} 
-          placeholder="Movie name" 
-          autoFocus 
-          autoComplete="off" 
-          aria-label="movie search title"
-          required 
-        />
+      onSubmit={handleSearch}
+    >
+      <input 
+        name="searchQuery" 
+        type="text" 
+        className={css["search-query-input"]} 
+        placeholder="Movie name" 
+        autoFocus 
+        autoComplete="off" 
+        aria-label="movie search title"
+        required 
+      />
       <button className={css["search-btn"]} type="submit">Search</button>
     </form>
   )
