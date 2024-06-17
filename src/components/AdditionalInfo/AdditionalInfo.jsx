50f73963deafd6591ed0b5138d0398b7a495f67e
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-17 22:03:29 +0200

    Add back button function for Cast and Reviews without context

--- src/components/AdditionalInfo/AdditionalInfo.jsx
+++ src/components/AdditionalInfo/AdditionalInfo.jsx
@@ -1,13 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import css from "./AdditionalInfo.module.css";
 
 const AdditionalInfo = () => {
+  const location = useLocation();
   return (
     <div className={css["additional-info"]}>
       <p>Additional info</p>
       <div className={css.options}>
-        <NavLink className={css.option} to="cast">Cast</NavLink>
-        <NavLink className={css.option} to="reviews">Reviews</NavLink>
+        <NavLink className={css.option} to="cast" state={{ from: location }}>Cast</NavLink>
+        <NavLink className={css.option} to="reviews" state={{ from: location }}>Reviews</NavLink>
       </div>
     </div>
   )
