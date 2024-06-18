1bc415883dc9630f362dd0892875c82a164647d7
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-18 10:29:04 +0200

    Add prop-types dependency

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import css from "./SearchForm.module.css";
 
 const SearchForm = ({onSearch}) => {
-  const handleSearch = (event) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchQueryChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const query = event.target.searchQuery.value;
-    onSearch(query);
+    onSearch(searchQuery);
+    setSearchQuery("");
     event.target.searchQuery.blur();
   };
 
   return (
     <form 
       className={css["search-form"]} 
-      onSubmit={handleSearch}
+      onSubmit={handleSubmit}
     >
       <input 
-        name="searchQuery" 
+        name="searchQuery"
+        onChange={handleSearchQueryChange}
+        value={searchQuery}
         type="text" 
         className={css["search-query-input"]} 
         placeholder="Movie name" 
