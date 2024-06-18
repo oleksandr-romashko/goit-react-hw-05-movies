c1f0b81ae59cc344333485eae8ece297177d9a44
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-18 15:56:55 +0200

    Add null check to MovieList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -5,31 +5,33 @@ import css from "./MoviesList.module.css";
 const MoviesList = ({movies}) => {
   const location = useLocation();
 
-  if (movies && movies.length === 0) {
-      return <Message text="We apologize, but we couldn't find any movies matching your request." textAlign="center" />;
+  if (!movies) {
+    return null;
   }
 
-  if (movies) {
-    return (
-      <ul className={css["movies-list"]}>
-        {movies.map(({id, title, release_date: releaseDate}) => {
-          const releaseYear = releaseDate && new Date(releaseDate).getFullYear();
-          return (
-              <li key={id}>
-                <Link 
-                  className={css["movie-item-link"]} 
-                  to={`/movies/${id}`}
-                  state={{ from: location }}
-                >
-                  {title} {releaseDate && `(${releaseYear})`}
-                </Link>
-              </li>
-            );
-          })
-        }
-      </ul>
-    )
+  if (movies && movies.length === 0) {
+    return <Message text="We apologize, but we couldn't find any movies matching your request." textAlign="center" />;
   }
+
+  return (
+    <ul className={css["movies-list"]}>
+      {movies.map(({id, title, release_date: releaseDate}) => {
+        const releaseYear = releaseDate && new Date(releaseDate).getFullYear();
+        return (
+            <li key={id}>
+              <Link 
+                className={css["movie-item-link"]} 
+                to={`/movies/${id}`}
+                state={{ from: location }}
+              >
+                {title}&nbsp;{releaseDate && `(${releaseYear})`}
+              </Link>
+            </li>
+          );
+        })
+      }
+    </ul>
+  )
 }
 
 export default MoviesList
