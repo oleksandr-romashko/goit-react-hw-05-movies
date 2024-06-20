bf238418a50f19831729bed7c82936ab29de2bca
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-20 12:13:46 +0200

    Add margin-top prop to Message component

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,29 @@
+import PropTypes from "prop-types";
 import css from "./Message.module.css";
+import React from "react";
 
-const Message = ({text, textAlign="center"}) => {
+/**
+ * Creates informational message.
+ * @param {string} props.text Informational text to display.
+ * @param {string} [props.textAlign="center"] Optional text alignment. By default is center.
+ * @param {string} props.marginTop Top margin value.
+ * @returns {React.Component} Informational message component.
+ */
+const Message = ({text, textAlign="center", marginTop}) => {
   return (
-    <p className={`${css["message-text"]} ${css[`align-${textAlign}`]}`}>{text}</p>
+    <p 
+      className={`${css["message-text"]} ${css[`align-${textAlign}`]}`} 
+      style = {{ marginTop: marginTop }}
+    >
+      {text}
+    </p>
   );
 };
 
 export default Message;
+
+Message.propTypes = {
+  text: PropTypes.string.isRequired,
+  textAlign: PropTypes.string,
+  marginTop: PropTypes.string,
+};
