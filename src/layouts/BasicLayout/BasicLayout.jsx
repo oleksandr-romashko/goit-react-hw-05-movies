de1be57151fc41d604f782bea960739a050c3f20
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-21 15:02:18 +0200

    Add BasicLayout js docs

--- src/layouts/BasicLayout/BasicLayout.jsx
+++ src/layouts/BasicLayout/BasicLayout.jsx
@@ -3,6 +3,10 @@ import { Outlet } from "react-router-dom";
 import { Header, Footer, Loader } from "components";
 import css from "./BasicLayout.module.css";
 
+/**
+ * Basic layout wrapper component.
+ * @returns {JSX.Element} Rendered layout component.
+ */
 const BasicLayout = () => {
   return (
     <>
