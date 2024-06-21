51b40b650b3291640851a913189d05b07eb8dcca
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-21 15:01:07 +0200

    Add js docs to preload imgs function

--- src/helpers/imagesPreload.js
+++ src/helpers/imagesPreload.js
@@ -2,12 +2,15 @@ import imgLeftArrow from "images/left-arrow.svg";
 import imgNoImage from "images/no-image.jpg";
 import imgSpool from "images/spool.svg";
 
-const preloadedImages = [
-  imgLeftArrow, 
-  imgNoImage,
-  imgSpool
-];
-
+/**
+ * Preloads and caches images.
+ */
 export function loadCriticalImages() {
+  const preloadedImages = [
+    imgLeftArrow, 
+    imgNoImage,
+    imgSpool
+  ];
+  
   preloadedImages.forEach(image => {new Image().src = image});
 }
