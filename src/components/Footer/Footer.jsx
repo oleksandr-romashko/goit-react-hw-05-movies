9d5bdc98d3592cdadfc39872144071f744a8fb76
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-21 14:19:44 +0200

    Fix footer and add js docs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,30 @@
 import { Container } from "components";
 import css from "./Footer.module.css";
 
+/**
+ * Footer component containing useful links and information.
+ * @returns {JSX.Element} Rendered footer component.
+ */
 const Footer = () => {
   return (
     <footer className={css["page-footer"]}>
       <Container>
         <div className={css.references}>
-          <p>
-            
-          </p>
           Credits: This web application uses 
           <a 
-              className={css["tmdb-reference-logo"]} 
-              href="https://www.themoviedb.org/"
-              title="The Movie Database"
-            >
-              <img 
-                className={css["tmdb-reference-logo"]}
-                src={require("images/attribution-tmdb.svg").default}
-                alt="the movie database logotype"
-                target="_blank"
-                rel="noopener noreferrer"
-              />
-            </a>
-          and the&nbsp;
+            className={css["tmdb-reference-logo"]} 
+            href="https://www.themoviedb.org/"
+            title="The Movie Database"
+          >
+            <img 
+              className={css["tmdb-reference-logo"]}
+              src={require("images/attribution-tmdb.svg").default}
+              alt="the movie database logotype"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+          </a>
+          <span>and the&nbsp;</span>
           <a 
             className={css["tmdb-reference-logo"]} 
             href="https://developer.themoviedb.org/docs/getting-started"
@@ -31,8 +32,8 @@ const Footer = () => {
           >
             TMDB APIs
           </a>
-          &nbsp;but is not endorsed, certified, or otherwise approved by TMDB.
-          </div>
+          <span>&nbsp;but is not endorsed, certified, or otherwise approved by TMDB.</span>
+        </div>
       </Container>
     </footer>
   )
