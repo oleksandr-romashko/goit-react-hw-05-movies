005ed3944832bcb5374751257bd1393e0a8ee4ad
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-21 13:38:37 +0200

    Add container prop-types and js docs

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types";
 import css from "./Container.module.css";
 
+/**
+ * Simple adaptive container wrapper. 
+ * @param {*} props.children Any children components. 
+ * @returns 
+ */
 const Container = ({children}) => {
   return (
     <div className={css.container}>{children}</div>
@@ -7,3 +13,7 @@ const Container = ({children}) => {
 };
 
 export default Container;
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.node]),
+}
