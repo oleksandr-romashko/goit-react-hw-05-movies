daddcfd390263fb2fb5767ba6b8b7109d628d2fb
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-06-21 14:43:14 +0200

    Add MoviesList prop-types and js docs

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,7 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
 import { Message } from "components";
 import css from "./MoviesList.module.css";
 
+/**
+ * Component with the list of movies.
+ * @param {object[]} props.movies Movies object array.
+ * @returns {JSX.Element} Rendered movies list component.
+ */
 const MoviesList = ({movies}) => {
   const location = useLocation();
 
@@ -10,8 +16,13 @@ const MoviesList = ({movies}) => {
   }
 
   if (movies && movies.length === 0) {
-    return <Message text="We apologize, but we couldn't find any movies matching your request." textAlign="center" />;
-  }
+    return (
+      <Message 
+        text="We apologize, but we couldn't find any movies matching your request." 
+        textAlign="center" 
+      />
+    );
+  };
 
   return (
     <ul className={css["movies-list"]}>
@@ -35,3 +46,7 @@ const MoviesList = ({movies}) => {
 }
 
 export default MoviesList
+
+MoviesList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
